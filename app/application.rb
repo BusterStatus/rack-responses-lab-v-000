8890c128b5cad427c563b79a0b5ddeee256a5018
matthew.serre <matthew.serre@gmail.com>
2019-05-25 23:30:52 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -3,7 +3,7 @@ class Application
   def call(env)
     resp = Rack::Response.new
     
-    if Time.now > 0 + 12.hours
+    if Time.now > 12
       resp.write "Good Afternoon!"
     else
       resp.write "Good Morning!"
